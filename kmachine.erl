d8f865cf5d5dc519b62d9ef05a0044d7daa53a62
Niklas Johansson <niklas.johansson@rte.se>
2016-02-26 11:42:01 +0100

    kmachine.erl: Use eval function to use rules kmachine_tr.erl: Isolated K-machine rules
    To improve the modularity and testability,
    we have split the original file in two.
    Each file includes test-cases.

--- kmachine.erl
+++ kmachine.erl
@@ -1,76 +1,44 @@
 -module(kmachine).
--export([tr/1]).
+-export([eval/1]).
+-import(kmachine_tr, [tr/1]).
 -include_lib("eunit/include/eunit.hrl").
 
-%% Background information
-%% In lambda calculus we sometimes write
-%% \x.x
-%% If we use deBruijn notation (nameless)
-%% we would write this as
-%% \.#0
-%% This can be compared to that
-%% \x.y is written as \.#1
+eval(State) ->
+    case kmachine_tr:tr(State) of
+        {stop, R} ->
+            R;
+        NewState ->
+            eval(NewState)
+    end.
+
 %%
-%% If we want to evaluate
-%% (\x.x \x.x)
-%% it would be written
-%% (\.#0 \.#0)
-%% And the correct evaluation is
-%% \.#
-%% or
-%% \x.x in the "standard" notation.
+%% TESTS
+%%
 
-%% Krivine's machine (K-machine)
-%% as described in the book
-%% Abstract Computing Machines
-%% by Kluge
-%% page 105-106
+-define(NIL, []).
+-define(V0, {deBruijn, 0}). %% #0
+-define(V1, {deBruijn, 1}). %% #1
 
+eval1_test_() ->
+    In = {?NIL, ?V0, ?NIL},
+    Out = eval(In),
+    Expected = ?V0,
+    ?_assert(Out =:= Expected).
 
-%% rule number 1
-tr({[{suspension, Env, Exp} | _E], {deBruijn, 0}, S}) ->
-    tr({ Env, Exp, S });
+-define(B0, {abstraction, ?V0}).      %% \.#0
+-define(A0, {application, ?B0, ?B0}). %% (\.#0 \.#0)
 
-%% rule number 2
-tr({[ _V | Env ], {deBruijn, N}, S}) ->
-    tr({ Env, {deBruijn, N-1}, S});
+eval2_test_() ->
+    In = {?NIL, ?A0, ?NIL},
+    Out = eval(In),
+    Expected = ?B0,
+    ?_assert(Out =:= Expected).
 
-%% rule number 3
-tr({Env, {application, E0, E1}, S}) ->
-    tr({ Env, E0, [{suspension, Env, E1}] ++ S});
+-define(B1, {abstraction, ?V1}).      %% \.#1
+-define(A1, {application, ?B0, ?B1}). %% (\.#0 \.#1)
 
-%% rule number 4
-tr({Env, {abstraction, Eb}, [ H | S]}) ->
-    tr({ [H] ++ Env, Eb, S});
-
-%% stop of machine, provide result
-tr({_E, R, []}) ->
-    R.
-
-
-%% Tests and example of the machine
-tr_test_() ->
-    NIL = [],
-
-    T0 = {deBruijn, 0},
-    T1 = {deBruijn, 1},
-
-    T2a = {abstraction, T0},
-    T2b = {application, T2a, T2a },
-
-    T3a = {abstraction, T1},
-    T3b = {application, T2a, T3a },
-
-    [
-     %% #0 -> #0
-     ?_assert(tr({NIL, T0, NIL}) =:= T0),
-
-     %% #1 -> #1
-     ?_assert(tr({NIL, T1, NIL}) =:= T1),
-
-     %% (\.#0 \.#0) -> \.#0
-     ?_assert(tr({NIL, T2b, NIL}) =:= T2a),
-
-     %% (\.#0 \.#1) -> \.#1
-     ?_assert(tr({NIL, T3b, NIL}) =:= T3a)
-    ].
+eval3_test_() ->
+    In = {?NIL, ?A1, ?NIL},
+    Out = eval(In),
+    Expected = ?B1,
+    ?_assert(Out =:= Expected).
